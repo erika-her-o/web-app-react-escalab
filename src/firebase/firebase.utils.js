5b4f4e09ece1877a5a3e3673298cfbbd89c4befc
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-12-01 22:46:40 -0300

    add import

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/app";
+import "firebase/firestore"; //functions
+import "firebase/auth"; //authentication
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+//config
+
+const config = {
   apiKey: "AIzaSyAmpqMl4fojanCRvmAEJ8ioH43s7o1T1Ww",
   authDomain: "bases-react-escalab-5c607.firebaseapp.com",
   projectId: "bases-react-escalab-5c607",
@@ -14,4 +14,39 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//create my web app with firebase
+firebase.initializeApp(config);
+
+//function for create-documents
+export const createUserProfileDocument = async (eserAuth, additionalData)=> {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uuid}`);
+
+    const snapShop = await userRef.get();
+
+    if(!snapShop.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName, email, createdAt, ... additionalData
+                
+            });
+        } catch (error) {
+            console.log("error creating users", error.message);
+        }
+    }
+    return userRef;
+}
+
+//exports
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//providers
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
