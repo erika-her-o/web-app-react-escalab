16231fe9c906f8f0482ffc9f49eddac43cabf2ad
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-12-01 21:21:19 -0300

    Instalations libraries and firebase config

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmpqMl4fojanCRvmAEJ8ioH43s7o1T1Ww",
+  authDomain: "bases-react-escalab-5c607.firebaseapp.com",
+  projectId: "bases-react-escalab-5c607",
+  storageBucket: "bases-react-escalab-5c607.appspot.com",
+  messagingSenderId: "1008443129250",
+  appId: "1:1008443129250:web:18968d3c82db76e7f6c6fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
