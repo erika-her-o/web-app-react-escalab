9ba3d9fa6c4fbc8e6599d9248a5970784505f5d8
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-12-06 17:22:17 -0300

    Add directory, shop data

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title,imageUrl, size, linkUrl }) => (
-    <div className={`${size}menu-item`}
-         onClick={()=>}
-    >
-        
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
-export default MenuItem;
+// design pattern: HOC (HIGH ORDER COMPONENT)
+export default withRouter(MenuItem);
